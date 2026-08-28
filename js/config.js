cb91a8dd408743e6fac800f70ca0a15bbd417cc2
bipalytara-byte <bipalytara@gmail.com>
2026-08-28 09:17:01 +0700

    Update Development Link

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 */
 
 
-const API = 'https://script.google.com/macros/s/AKfycbzcIoEqRDMV0rnNzPn6A_A8KP4JR_9hnQuKKY4yQDpvQq6p_M2mlenyjt1xJ9KCPtbN/exec';
+const API = 'https://script.google.com/macros/s/AKfycbx1DqAKM2pgkQtdrzW08JPwMjV1JnDHxY517bvcjWMeBQVtEHSBiLpoRtVTdPZWsEnYcA/exec';
 const HARI = ['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu'];
 let D=[],J=[],P=[],G=[],M=[],MK=[];
 let eDos=null,eJad=null,eMk=null,tempMk=[];
